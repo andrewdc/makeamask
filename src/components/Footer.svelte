0dba5eaf6c15d6454ab0106c08a404403657c9f6
Andrew Colclough <andrew.colclough@exaktime.com>
2020-03-25 10:26:41 -0500

    adds faq link in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -21,5 +21,6 @@
 <footer>
  <p><strong>A message from Jennifer:</strong></p>
  <p><em>There are not words I can put down on paper that could possibly express my gratitude to all of those that are helping with this project.  I have shared the exciting news with my own employees at Victory Medical and networked with dozens and dozens of clinics already to find out their needs and coordinate a distribution plan. Thank you for HELPING US… HELP YOU. May all your families stay safe and in good health. My thoughts and well wishes are with each of you and your families. <br> </em>🙏😷❤😊</p>
+ <p><a href="/faq">Frequently Asked Questions</a> </p>
  <img style="width:50px;"  src="/icons/008-clothes-3.svg" alt="needle and thread">
 </footer>

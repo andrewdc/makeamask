19be73f9d499b86d27eae35b8378d403deb63ea6
Andrew Colclough <andrew.colclough@exaktime.com>
2020-03-31 09:12:55 -0500

    testing FB api

---
+++ src/routes/gallery.svelte
@@ -0,0 +1,30 @@
+<script>
+import {onMount} from 'svelte';
+
+onMount(() => {
+FB.api(
+    "/119599209673746/photos?fields=images",
+    function (response) {
+      if (response && !response.error) {
+        console.log(response);
+      }
+    }
+);
+});
+
+</script>
+
+<style>
+  h5 {
+    color: grey;
+  }
+
+  .centrify {
+    text-align: center;
+  }
+</style>
+
+<div class="centrify">
+  <h5>Hello bruh!</h5>
+  
+</div>
